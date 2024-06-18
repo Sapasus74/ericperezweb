9d8eb827bccef890850b88708ade6addb0accfd6
ericperezartero <69244853+Sapasus74@users.noreply.github.com>
2024-06-18 16:29:02 +0200

    Add files via upload

--- cambiarIdioma.js
+++ cambiarIdioma.js
@@ -5,9 +5,8 @@ var translations = {
         "sobreMiMenu": "[Sobre Mi]",
         "proyectosMenu" :"[Proyectos]",
         "conociminetosMenu" : "[Conociminetos]",
-        "contactoMenu": "[Contacto]",
         "informacionHola": "Hola, mi nombre es [Eric Perez]",
-        "informacionCiberseguridad": "apasionada por la ciberseguridad",
+        "informacionCiberseguridad": "apasionado por la ciberseguridad",
         "sobreMi": "[Sobre Mi]",
         "aboutMe": "Soy un estudiante de Administración de Sistemas Informáticos en Red (ASIR). Me apasiona el mundo de la ciberseguridad y el hacking ético y estoy comprometido con explorar sus dificultades y desafíos. Desde que entré en el mundo de la informática, tuve claro que lo que me interesaba estaba en asegurar sistemas y proteger la información digital.",
         "proyectos":"[Proyectos]",
@@ -19,7 +18,6 @@ var translations = {
         "sobreMiMenu": "[About Me]",
         "proyectosMenu" :"[Projects]",
         "conociminetosMenu" : "[Knowledge]",
-        "contactoMenu": "[Contact]",
         "informacionHola": "Hi, my name is [Eric Perez]",
         "informacionCiberseguridad": "passionate about cybersecurity",
         "sobreMi": "[About Me]",
@@ -36,7 +34,6 @@ function changeLanguage() {
     document.getElementById("sobreMi-menu").innerText = translations[language]["sobreMiMenu"];
     document.getElementById("proyectos-menu").innerText = translations[language]["proyectosMenu"];
     document.getElementById("conociminetos-menu").innerText = translations[language]["conociminetosMenu"];
-    document.getElementById("contacto-menu").innerText = translations[language]["contactoMenu"];
     document.getElementById("informacion-hola").innerText = translations[language]["informacionHola"];
     document.getElementById("informacion-ciberseguridad").innerText = translations[language]["informacionCiberseguridad"];
     document.getElementById("sobre-mi").innerText = translations[language]["sobreMi"];
